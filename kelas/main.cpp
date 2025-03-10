01695f5ee390d712a66c6f09cd4e4226133150bd
MuhammadRivaFachrodhiya <muhammadrivaf@gmail.com>
2025-03-10 10:08:53 +0800

    Upload File

---
+++ kelas/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     // bool sudahMenikah = true;
+//     // string status = sudahMenikah ? "Sudah Menikah" : " Belum Menikah";
+//     // cout << status << endl;
+//     bool malas = true;
+//     bool lapar = true;
+//     if (malas) {
+//      if (lapar) {
+//         cout << "Saya makan";
+//         }
+//     } else {
+//         cout << "Gak ngapa-ngapain";
+//         }
+
+    
+//     return 0;
+// }
+
+// int main()
+// {
+// for (int i = 5; i > 0 ; i--) {
+//  cout << i << endl;
+// }
+
+// }
